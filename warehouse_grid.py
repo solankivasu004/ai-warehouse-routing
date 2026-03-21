b8b8960859a631352b8e804b97cca858c222cc94
VASUDEV <vasudevsolanki478@gmail.com>
2026-03-21 17:49:52 +0530

    integrate A* algorithm for optimal pathfinding in warehouse grid

--- warehouse_grid.py
+++ warehouse_grid.py
@@ -1,20 +1,18 @@
+from robot import Robot, get_neighbors
 from astar import astar
 
 class WarehouseGrid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        
-        # Create empty grid
+
         self.grid = [["." for _ in range(width)] for _ in range(height)]
-        
-        # Define start and goal
+
         self.start = (0, 0)
-        self.goal = (height-1, width-1)
-        
-        # Add obstacles
-        self.obstacles = [(1,2), (2,2), (3,2), (3,3)]
-        
+        self.goal = (height - 1, width - 1)
+
+        self.obstacles = [(1, 2), (2, 2), (3, 2), (3, 3)]
+
         for x, y in self.obstacles:
             self.grid[x][y] = "#"
 
@@ -24,15 +22,12 @@ class WarehouseGrid:
 
 
 if __name__ == "__main__":
-    
-    # Create warehouse
-    warehouse = WarehouseGrid(6,6)
+    warehouse = WarehouseGrid(6, 6)
     warehouse.display_grid()
 
     print("\nStart:", warehouse.start)
     print("Goal:", warehouse.goal)
 
-    # Run A* algorithm
     path = astar(warehouse.grid, warehouse.start, warehouse.goal)
 
     print("\nOptimal Path:", path)
